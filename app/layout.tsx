dcf231b2b381844d3c07367fe1311b75f9ef87b4
adam <47948089+adambarjo@users.noreply.github.com>
2024-02-05 13:24:50 +0000

    init

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import { classNames } from "@/utils";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Adam Jones",
+  description: "Adam Jones portfolio site",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={classNames(inter.className)}>{children}</body>
+    </html>
+  );
+}
